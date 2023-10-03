1cd75c64a27796ee056ccd9f3a6aea8c98dee4c9
Dan Luoma <dan@danluoma.com>
2023-10-02 18:42:29 -0700

    3

--- src/main/java/TranPackage/Transaction.java
+++ src/main/java/TranPackage/Transaction.java
@@ -30,88 +30,45 @@ public class Transaction {
         createConnection();
     }
 
-    //Trying throwing an error message. Trying to have thrown from CockroachHandler constructor
     private void createConnection() throws SQLException{
-        try {
-            conn = ds.getConnection();
-        } catch (SQLException e) {
-            throw new SQLException(e.getMessage() + ". Please correct error and try again.");
-        }
+        conn = ds.getConnection();
     }
 
-    public Connection getConnection(){
-        try {
-            if (conn == null || conn.isClosed())
-                createConnection();
-        } catch (SQLException e) {
-            printSQLError(e, "getConnection");
-        }
+    public Connection getConnection() throws SQLException{
+        if (conn == null || conn.isClosed())
+            createConnection();
 
         return conn;
     }
 
     public void start() throws SQLException{
         createConnection();
-        try {
-            conn.setAutoCommit(false);
-        } catch (SQLException e) {
-            printSQLError(e, "start");
-        }
+        conn.setAutoCommit(false);
     }
 
-    public void end(){
+    public void end() throws SQLException{
         try {
             conn.commit();
         } catch (SQLException e) {
-            try {
-                close();
-            } catch (Exception ee) {
-                printError(ee, "end (after failed commit and failed close)");
-            }
-            printSQLError(e, "end (after failed commit and successful close)");
+            close();
         }
 
-        try {
-            close();
-        } catch (Exception e) {
-            printError(e, "end (after successful commit and failed close)");
-        }
+        close();
     }
 
-    public void rollback(){
+    public void rollback() throws SQLException{
         try {
             conn.rollback();
         } catch (SQLException e) {
-            try {
-                close();
-            } catch (Exception ee) {
-                printError(ee, "rollback (after failed rollback and failed close)");
-            }
-            printSQLError(e, "rollback (after failed rollback and successful close)");
+            close();
         }
     
-        try {
-            close();
-        } catch (Exception e) {
-            printError(e, "rollback (after successful rollback and failed close)");
-        }
+        close();
     }
 
-    private void close(){
-        try {
-            conn.close();
-            conn = null;
-        } catch (SQLException e) {
-            printSQLError(e, "close");
-        }
-    }
-
-    private void printSQLError(SQLException e, String source){
-        System.out.printf("ToDoTransaction.%s SQLERROR: { state => %s, cause => %s, message => %s }\n", source ,e.getSQLState(), e.getCause(), e.getMessage());
-    }
-
-    private void printError(Exception e, String source){
-        System.out.printf("ToDoTransaction.%s ERROR: { cause => %s, message => %s }\n", source, e.getCause(), e.getMessage());
+    private void close() throws SQLException{
+        conn.close();
+        conn = null;
 
     }
 }
