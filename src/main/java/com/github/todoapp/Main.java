f22b759f2a4458eb1258f4b0d9b7102e388636f0
Dan Luoma <dan@danluoma.com>
2023-10-02 19:21:32 -0700

    Fixed logging issue

--- src/main/java/com/github/todoapp/Main.java
+++ src/main/java/com/github/todoapp/Main.java
@@ -1,12 +1,25 @@
 package com.github.todoapp;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import ControllerPackage.ToDoController;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    
+    public static void main(String[] args) {
+        FileHandler fh = null;
+        try {
+            fh = new FileHandler("ErrorLog.log");
+        } catch (SecurityException | IOException e) {
+            e.printStackTrace();
+        }
+        fh.setFormatter(new SimpleFormatter());
+        LOGGER.addHandler(fh);
+
         ToDoController ctr = new ToDoController();
         ctr.start();
     }
